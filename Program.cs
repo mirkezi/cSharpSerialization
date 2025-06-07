009750b63da3b3ae7cf0d160f0304409b44be092
mirkezi <mirko.venali@gmail.com>
2025-06-07 15:03:28 +0200

    program.cs file inizialized

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using System.Text.Json;
+
+public class Program
+{
+    public class Person
+    {
+        public string UserName { get; set; }
+        public int UserAge { get; set; }
+    }
+
+    static void Main()
+    {
+        //Instantiate new Person object
+
+        // Binary Serialization
+
+        // Binary Deserialization
+
+        // XML Serialization
+
+        // XML Deserialization
+
+        // Json Serialization
+
+        // Json Deserialization
+
+
+    }
+}
