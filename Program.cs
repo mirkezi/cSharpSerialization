aad0b655c8b441581d3b17a9efb30395b2a92ebd
mirkezi <mirko.venali@gmail.com>
2025-06-07 15:19:40 +0200

    binary serialization/deserialization implemented

--- Program.cs
+++ Program.cs
@@ -13,11 +13,42 @@ public class Program
     static void Main()
     {
         //Instantiate new Person object
+        var mirko = new Person { UserName = "mirko", UserAge = 33 };
 
         // Binary Serialization
+        try
+        {
+            using (var fs = new FileStream("person.dat", FileMode.Create))
+            using (var writer = new BinaryWriter(fs))
+            {
+                writer.Write(mirko.UserName);
+                writer.Write(mirko.UserAge);
+                Console.WriteLine("Binary serialization completed.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Binary serialization failed: {ex.Message}");
+        }
 
         // Binary Deserialization
-
+        try
+        {
+            using (var fs = new FileStream("person.dat", FileMode.Open))
+            using (var reader = new BinaryReader(fs))
+            {
+                var deserializedPerson1 = new Person
+                {
+                    UserName = reader.ReadString(),
+                    UserAge = reader.ReadInt32(),
+                };
+                Console.WriteLine($"Binary deserialization completed. Name: {deserializedPerson1.UserName} , Age: {deserializedPerson1.UserAge}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Binary deserialization failed: {ex.Message}");
+        }
         // XML Serialization
 
         // XML Deserialization
